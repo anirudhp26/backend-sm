28aea619c175f62969d555c7eef7d5f06d340f35
Anirudh Patel <anirudh26p@gmail.com>
2023-02-07 18:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // session saving and cookie management
 app.use(cors({
-    origin: ["*"],
+    origin: ["http://localhost:3000","https://test-4e8c8.web.app"],
     methods: ["GET", "POST"],
     credentials: true,
     cross
